ce2ab23baab750481c067bb64f295e6589e601d1
digocz <digo_zaparoli@hotmail.com>
2022-09-28 18:05:49 -0300

    Update TestSort.c

--- Unity/foo/test/TestSort.c
+++ Unity/foo/test/TestSort.c
@@ -2,6 +2,10 @@
 #include "unity.h"
 #include "unity_fixture.h"
 
+
+	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int length = sizeof(a) / sizeof(int);
+
 TEST_GROUP(Sort);
 
 TEST_SETUP(Sort)
@@ -15,7 +19,7 @@ TEST_TEAR_DOWN(Sort)
 TEST(Sort, TestSort1)
 {
   // All of these should pass
-  //TEST_ASSERT_EQUAL(-2, foo(1,2));
+  TEST_ASSERT_EQUAL(0, sort(a, length, "On2", SELECTION));
   //TEST_ASSERT_EQUAL(-1, foo(2,2));
 }
 
