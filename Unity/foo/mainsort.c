e8e23df1c1cc926e940530c8f47e1e7afd86bb8b
digocz <digo_zaparoli@hotmail.com>
2022-09-28 19:58:31 -0300

    Update mainsort.c

--- Unity/foo/mainsort.c
+++ Unity/foo/mainsort.c
@@ -8,8 +8,7 @@ int mainsort(void)
 	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int length = sizeof(a) / sizeof(int);
 	
-	printf("Unsorted: \n");
-	print_array(a, length);
+	
 
 	int status = sort(a, length, "On2", SELECTION);
 	printf("Status: %d\n", status);
