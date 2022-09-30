4ee6a6147255d48610c88287d312746b728f2cb3
digocz <digo_zaparoli@hotmail.com>
2022-09-30 19:20:19 -0300

    Update TestSort.c

--- Unity/foo/test/TestSort.c
+++ Unity/foo/test/TestSort.c
@@ -33,8 +33,9 @@ TEST(Sort, TestSort2) //TESTE DE VALOR LIMITE MINIMO
 	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, INT_MIN};
 	int b[] ={INT_MIN,2,3,4,5,6,7,8,9};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
 	
-  TEST_ASSERT_EQUAL(0, sort(a, length, "On2", SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, SELECTION));
   TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
 
 	
@@ -46,8 +47,9 @@ TEST(Sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
 	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 0};
 	int b[] ={0,2,3,4,5,6,7,8,9};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
 	
-  TEST_ASSERT_EQUAL(0, sort(a, length, "On2", SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, SELECTION));
   TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
 	
 	
@@ -58,8 +60,10 @@ TEST(Sort, TestSort4) //TESTE ARRAY ULTRAPASSANDO VALOR MAXIMO
 		int a[] = {21,20,19,18,17,16,15,14,13,12,11,10,9, 8, 7, 6, 5, 4, 3, 2, 1};
 	//int b[] ={1,2,3,4,5,6,7,8,INT_MAX*INT_MAX};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
 	
- TEST_ASSERT_EQUAL(1, sort(a, length, "On2", SELECTION));
+	
+ TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
  // TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
 	
 	
@@ -69,8 +73,9 @@ TEST(Sort, TestSort5)
 {
 	int a[] = {-255, 8, 7, 6, 5, 4, 3, 2, 1};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
   // All of these should pass
-  TEST_ASSERT_EQUAL(0, sort(a, length, "On2", SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, SELECTION));
 	
 	
 }
@@ -80,8 +85,9 @@ TEST(Sort, TestSort6)
 	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int b[] ={1,2,3,4,5,6,7,8,9};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
 	
-  TEST_ASSERT_EQUAL(0, sort(a, length, "On2", SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, SELECTION));
   TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
 
 	
@@ -92,8 +98,9 @@ TEST(Sort, TestSort7)
 {
 	int a[] = {-255, 8, 7, 6, 5, 4, 3, 2, 1};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
   // All of these should pass
-  TEST_ASSERT_EQUAL(0, sort(a, length, "On2", SELECTION));
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, SELECTION));
 	
 	
 }
