df35bc7b2273ff9920f1c1883550e8a7e1e66a85
digocz <digo_zaparoli@hotmail.com>
2022-10-01 12:21:50 -0300

    Update TestSort_Runner.c

--- Unity/foo/test/test_runners/TestSort_Runner.c
+++ Unity/foo/test/test_runners/TestSort_Runner.c
@@ -58,3 +58,18 @@ TEST_GROUP_RUNNER(Counting_sort)
   RUN_TEST_CASE(Counting_sort, TestSort9);
   RUN_TEST_CASE(Counting_sort, TestSort10);
 }
+
+
+TEST_GROUP_RUNNER(Radix_sort)
+{
+  RUN_TEST_CASE(Radix_sort, TestSort1);
+  RUN_TEST_CASE(Radix_sort, TestSort2);
+  RUN_TEST_CASE(Radix_sort, TestSort3);
+  RUN_TEST_CASE(Radix_sort, TestSort4);
+  RUN_TEST_CASE(Radix_sort, TestSort5);
+  RUN_TEST_CASE(Radix_sort, TestSort6);
+  RUN_TEST_CASE(Radix_sort, TestSort7);
+  RUN_TEST_CASE(Radix_sort, TestSort8);
+  RUN_TEST_CASE(Radix_sort, TestSort9);
+  RUN_TEST_CASE(Radix_sort, TestSort10);
+}
