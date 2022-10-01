a78215323c14a6c02e14110004609a0bcbb6624f
digocz <digo_zaparoli@hotmail.com>
2022-10-01 12:35:48 -0300

    Update TestCountingsort.c

--- Unity/foo/test/TestCountingsort.c
+++ Unity/foo/test/TestCountingsort.c
@@ -63,6 +63,15 @@ TEST(Counting_sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
 
 TEST(Counting_sort, TestSort4) //TESTE ARRAY ULTRAPASSANDO VALOR MAXIMO
 {
+	int a[] = {21,20,19,18,17,16,15,14,13,12,11,10,9, 8, 7, 6, 5, 4, 3, 2, 1};
+	//int b[] ={1,2,3,4,5,6,7,8,INT_MAX*INT_MAX};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On";
+	
+	
+ TEST_ASSERT_EQUAL(1, sort(a, length, complex, COUNTING));
+ // TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+	
 
 	
 	
@@ -70,6 +79,14 @@ TEST(Counting_sort, TestSort4) //TESTE ARRAY ULTRAPASSANDO VALOR MAXIMO
 
 TEST(Counting_sort, TestSort5)
 {
+	
+	int a[] = {-255, 8, 7, 6, 5, 4, 3, 2, 1};
+	int b[] ={-255,0,2,3,4,5,6,7,8};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On";
+  // All of these should pass
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, COUNTING));
+  TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
 
 	
 }
