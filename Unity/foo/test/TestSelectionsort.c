4ca00548378067825e852b86ac82dd60e619e5da
Razzmann <53624104+Razzmann@users.noreply.github.com>
2022-10-01 20:09:19 -0300

    Update TestSelectionsort.c

--- Unity/foo/test/TestSelectionsort.c
+++ Unity/foo/test/TestSelectionsort.c
@@ -55,16 +55,16 @@ TEST(Selection_sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
 	
 }
 
-TEST(Selection_sort, TestSort4) //TESTE ARRAY ULTRAPASSANDO VALOR MAXIMO
+TEST(Selection_sort, TestSort4) //TESTE DE VALORES REPETIDOS E EM ORDEM
 {
-		int a[] = {21,20,19,18,17,16,15,14,13,12,11,10,9, 8, 7, 6, 5, 4, 3, 2, 1};
-	//int b[] ={1,2,3,4,5,6,7,8,INT_MAX*INT_MAX};
+	int a[] = {0 , 0, 1, 1, 2, 2, 3, 3, 4, 4};
+	int b[] = {0 , 0, 1, 1, 2, 2, 3, 3, 4, 4};
 	int length = sizeof(a) / sizeof(int);
 	char complex[]="On2";
 	
 	
- TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
- // TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+ TEST_ASSERT_EQUAL(0, sort(a, length, complex, SELECTION));
+ TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);	
 	
 	
 }
@@ -108,17 +108,33 @@ TEST(Selection_sort, TestSort7)//TESTE DE VALORES IGUAIS
 	
 }
 
-TEST(Selection_sort, TestSort8)
+TEST(Selection_sort, TestSort8)//TESTE ARRAY OVERFLOW
 {
-
+	int a[] = {21,20,19,18,17,16,15,14,13,12,11,10,9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+	
+ TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
+	
+	
+	
 }
-TEST(Selection_sort, TestSort9)
+TEST(Selection_sort, TestSort9)//TESTE ARRAY UNDERFLOW
 {
-
+	int a[] = {211};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+	
+ TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
 }
-TEST(Selection_sort, TestSort10)
+TEST(Selection_sort, TestSort10)//TESTE ARRAY ZERADO
 {
-
+	int a[] = {};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	 TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
 }
 
 
