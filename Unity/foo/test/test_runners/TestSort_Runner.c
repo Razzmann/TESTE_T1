4f2075eefbf081e62767c0bf56f61fc5bb300679
digocz <digo_zaparoli@hotmail.com>
2022-09-30 21:28:34 -0300

    Update TestSort_Runner.c

--- Unity/foo/test/test_runners/TestSort_Runner.c
+++ Unity/foo/test/test_runners/TestSort_Runner.c
@@ -14,3 +14,18 @@ TEST_GROUP_RUNNER(Sort)
   RUN_TEST_CASE(Sort, TestSort9);
   RUN_TEST_CASE(Sort, TestSort10);
 }
+
+
+TEST_GROUP_RUNNER(Bubble_sort)
+{
+  RUN_TEST_CASE(Bubble_sort, TestSort1);
+  RUN_TEST_CASE(Bubble_sort, TestSort2);
+  RUN_TEST_CASE(Bubble_sort, TestSort3);
+  RUN_TEST_CASE(Bubble_sort, TestSort4);
+  RUN_TEST_CASE(Bubble_sort, TestSort5);
+  RUN_TEST_CASE(Bubble_sort, TestSort6);
+  RUN_TEST_CASE(Bubble_sort, TestSort7);
+  RUN_TEST_CASE(Bubble_sort, TestSort8);
+  RUN_TEST_CASE(Bubble_sort, TestSort9);
+  RUN_TEST_CASE(Bubble_sort, TestSort10);
+}
