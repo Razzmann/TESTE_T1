b22686975c1675c59840f56b8c90eb8435890366
Razzmann <53624104+Razzmann@users.noreply.github.com>
2022-10-01 20:33:39 -0300

    Update TestSelectionsort.c

--- Unity/foo/test/TestSelectionsort.c
+++ Unity/foo/test/TestSelectionsort.c
@@ -184,9 +184,28 @@ TEST(Selection_sort, TestSort15)//TESTE COMPLEXIDADE ERRADA
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
 
 }
-TEST(Selection_sort, TestSort16){}
-TEST(Selection_sort, TestSort17){}
-TEST(Selection_sort, TestSort18){}
+TEST(Selection_sort, TestSort16)//TESTE COMPLEXIDADE ERRADA
+{
+	int a[] = {-176234,561,1112,-5671,85124,5819};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="";
+	
+	
+ TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
+}
+TEST(Selection_sort, TestSort17)//TESTE COMPLEXIDADE ERRADA
+{
+	int a[] = {-176234,561,1112,-5671,85124,5819};
+	int length = sizeof(a) / sizeof(int);
+	char complex[];
+	
+	
+ TEST_ASSERT_EQUAL(1, sort(a, length, complex, SELECTION));
+}
+
+TEST(Selection_sort, TestSort18)
+{
+}
 TEST(Selection_sort, TestSort19){}
 TEST(Selection_sort, TestSort20){}
 
