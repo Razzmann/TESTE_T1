b18ac711dd97aaa841380086b776f32f42dc1d6e
digocz <digo_zaparoli@hotmail.com>
2022-10-01 12:21:20 -0300

    Update all_tests.c

--- Unity/foo/test/test_runners/all_tests.c
+++ Unity/foo/test/test_runners/all_tests.c
@@ -6,6 +6,7 @@ static void RunAllTests(void)
   RUN_TEST_GROUP(Bubble_sort);
   RUN_TEST_GROUP(Insertion_sort);
   RUN_TEST_GROUP(Counting_sort);
+  RUN_TEST_GROUP(Radix_sort);
 }
 
 int main(int argc, const char * argv[])
