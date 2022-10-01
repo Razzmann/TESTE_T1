7edb2db51adc22059e0f2046ab445795fd612f70
digocz <digo_zaparoli@hotmail.com>
2022-09-30 21:25:54 -0300

    Create TestBubblesort_Runner.c

---
+++ Unity/foo/test/test_runners/TestBubblesort_Runner.c
@@ -0,0 +1,126 @@
+#include "Bubble_sort.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+
+	
+
+TEST_GROUP(Bubble_sort);
+
+TEST_SETUP(Bubble_sort)
+{
+
+}
+
+TEST_TEAR_DOWN(Bubble_sort)
+{
+}
+
+TEST(Bubble_sort, TestSort1) //TESTE DE VALOR LIMITE MAXIMO
+{ 
+	int a[] = {INT_MAX, 8, 7, 6, 5, 4, 3, 2, 1};
+	int b[] ={1,2,3,4,5,6,7,8,INT_MAX};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+  TEST_ASSERT_EQUAL(0, Bubble_sort(a, length, complex, BUBBLE));
+  TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+
+}
+
+TEST(Bubble_sort, TestSort2) //TESTE DE VALOR LIMITE MINIMO
+{
+	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, INT_MIN};
+	int b[] ={INT_MIN,2,3,4,5,6,7,8,9};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+  TEST_ASSERT_EQUAL(0, Bubble_sort(a, length, complex, BUBBLE));
+  TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+
+	
+	
+}
+
+TEST(Bubble_sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
+{
+	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 0};
+	int b[] ={0,2,3,4,5,6,7,8,9};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+  TEST_ASSERT_EQUAL(0, Bubble_sort(a, length, complex, BUBBLE));
+  TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+	
+	
+}
+
+TEST(Bubble_sort, TestSort4) //TESTE ARRAY ULTRAPASSANDO VALOR MAXIMO
+{
+		int a[] = {21,20,19,18,17,16,15,14,13,12,11,10,9, 8, 7, 6, 5, 4, 3, 2, 1};
+	//int b[] ={1,2,3,4,5,6,7,8,INT_MAX*INT_MAX};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+	
+ TEST_ASSERT_EQUAL(1, Bubble_sort(a, length, complex, BUBBLE));
+ // TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+	
+	
+}
+
+TEST(Bubble_sort, TestSort5)
+{
+	int a[] = {-255, 8, 7, 6, 5, 4, 3, 2, 1};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+  // All of these should pass
+  TEST_ASSERT_EQUAL(0, Bubble_sort(a, length, complex, BUBBLE));
+	
+	
+}
+
+TEST(Bubble_sort, TestSort6)
+{
+	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int b[] ={1,2,3,4,5,6,7,8,9};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+	
+  TEST_ASSERT_EQUAL(0, Bubble_sort(a, length, complex, BUBBLE));
+  TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+
+	
+
+	
+}
+TEST(Bubble_sort, TestSort7)
+{
+	int a[] = {-255, 8, 7, 6, 5, 4, 3, 2, 1};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
+  // All of these should pass
+  TEST_ASSERT_EQUAL(0, Bubble_sort(a, length, complex, BUBBLE));
+	
+	
+}
+
+TEST(Bubble_sort, TestSort8)
+{
+
+}
+TEST(Bubble_sort, TestSort9)
+{
+
+}
+TEST(Bubble_sort, TestSort10)
+{
+
+}
+
+
+
+
+
+
+
