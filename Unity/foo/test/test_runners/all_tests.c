14049735c2a75eeeadfabfd9bd0e36c79a2a4d6d
Razzmann <53624104+Razzmann@users.noreply.github.com>
2022-10-02 00:26:56 -0300

    Update all_tests.c

--- Unity/foo/test/test_runners/all_tests.c
+++ Unity/foo/test/test_runners/all_tests.c
@@ -3,11 +3,12 @@
 static void RunAllTests(void)
 {
   RUN_TEST_GROUP(Selection_sort);
-/*
+
   RUN_TEST_GROUP(Bubble_sort);
   RUN_TEST_GROUP(Insertion_sort);
   RUN_TEST_GROUP(Counting_sort);
   RUN_TEST_GROUP(Radix_sort);
+  /*
   RUN_TEST_GROUP(Merge_sort);
   RUN_TEST_GROUP(Heap_sort);
   RUN_TEST_GROUP(Quick_sort);
