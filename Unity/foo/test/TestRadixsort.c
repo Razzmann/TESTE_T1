fd88d1f5cf7c4e5cd3905b4b36d7c4cf3f343171
digocz <digo_zaparoli@hotmail.com>
2022-10-02 10:45:44 -0300

    Update TestRadixsort.c

--- Unity/foo/test/TestRadixsort.c
+++ Unity/foo/test/TestRadixsort.c
@@ -94,13 +94,13 @@ TEST(Radix_sort, TestSort5)//TESTE DE VALOR LIMITE metade de min e max
 TEST(Radix_sort, TestSort6)//TESTE DE VALORES JÁ ORDENADOS.  -- FALHA! (FAIL: Element 1 Expected 125 Was 345)
 {
 
-	//int a[] = {-767, 125, 345 , 367, 421, 5743, 6612, 7012, 8999,9999 , 10000, 11142, 12928,91023,123507,1681223};
-	//int b[] = {-767, 125, 345 , 367, 421, 5743, 6612, 7012, 8999,9999 , 10000, 11142, 12928,91023,123507,1681223};
-	//int length = sizeof(a) / sizeof(int);
-	//char complex[]="On";
+	int a[] = {-767, 125, 345 , 367, 421, 5743, 6612, 7012, 8999,9999 , 10000, 11142, 12928,91023,123507,1681223};
+	int b[] = {-767, 125, 345 , 367, 421, 5743, 6612, 7012, 8999,9999 , 10000, 11142, 12928,91023,123507,1681223};
+	int length = sizeof(a) / sizeof(int);
+	char complex[]="On";
 	
- // TEST_ASSERT_EQUAL(0, sort(a, length, complex, RADIX));
- // TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
+  TEST_ASSERT_EQUAL(0, sort(a, length, complex, RADIX));
+  TEST_ASSERT_EQUAL_INT_ARRAY(b, a, length);
 
 	
 
