b8913f058d0c883386b9200dc580f867169e480e
Razzmann <53624104+Razzmann@users.noreply.github.com>
2022-10-01 22:34:51 -0300

    Update TestSort_Runner.c

--- Unity/foo/test/test_runners/TestSort_Runner.c
+++ Unity/foo/test/test_runners/TestSort_Runner.c
@@ -38,6 +38,16 @@ TEST_GROUP_RUNNER(Bubble_sort)
   RUN_TEST_CASE(Bubble_sort, TestSort8);
   RUN_TEST_CASE(Bubble_sort, TestSort9);
   RUN_TEST_CASE(Bubble_sort, TestSort10);
+  RUN_TEST_CASE(Bubble_sort, TestSort11);
+  RUN_TEST_CASE(Bubble_sort, TestSort12);
+  RUN_TEST_CASE(Bubble_sort, TestSort13);
+  RUN_TEST_CASE(Bubble_sort, TestSort14);
+  RUN_TEST_CASE(Bubble_sort, TestSort15);
+  RUN_TEST_CASE(Bubble_sort, TestSort16);
+  RUN_TEST_CASE(Bubble_sort, TestSort17);
+  RUN_TEST_CASE(Bubble_sort, TestSort18);
+  RUN_TEST_CASE(Bubble_sort, TestSort19);
+  RUN_TEST_CASE(Bubble_sort, TestSort20);
 }
 
 
@@ -53,6 +63,16 @@ TEST_GROUP_RUNNER(Insertion_sort)
   RUN_TEST_CASE(Insertion_sort, TestSort8);
   RUN_TEST_CASE(Insertion_sort, TestSort9);
   RUN_TEST_CASE(Insertion_sort, TestSort10);
+  RUN_TEST_CASE(Insertion_sort, TestSort11);
+  RUN_TEST_CASE(Insertion_sort, TestSort12);
+  RUN_TEST_CASE(Insertion_sort, TestSort13);
+  RUN_TEST_CASE(Insertion_sort, TestSort14);
+  RUN_TEST_CASE(Insertion_sort, TestSort15);
+  RUN_TEST_CASE(Insertion_sort, TestSort16);
+  RUN_TEST_CASE(Insertion_sort, TestSort17);
+  RUN_TEST_CASE(Insertion_sort, TestSort18);
+  RUN_TEST_CASE(Insertion_sort, TestSort19);
+  RUN_TEST_CASE(Insertion_sort, TestSort20);
 }
 
 TEST_GROUP_RUNNER(Counting_sort)
@@ -67,6 +87,16 @@ TEST_GROUP_RUNNER(Counting_sort)
   RUN_TEST_CASE(Counting_sort, TestSort8);
   RUN_TEST_CASE(Counting_sort, TestSort9);
   RUN_TEST_CASE(Counting_sort, TestSort10);
+  RUN_TEST_CASE(Counting_sort, TestSort11);
+  RUN_TEST_CASE(Counting_sort, TestSort12);
+  RUN_TEST_CASE(Counting_sort, TestSort13);
+  RUN_TEST_CASE(Counting_sort, TestSort14);
+  RUN_TEST_CASE(Counting_sort, TestSort15);
+  RUN_TEST_CASE(Counting_sort, TestSort16);
+  RUN_TEST_CASE(Counting_sort, TestSort17);
+  RUN_TEST_CASE(Counting_sort, TestSort18);
+  RUN_TEST_CASE(Counting_sort, TestSort19);
+  RUN_TEST_CASE(Counting_sort, TestSort20);
 }
 
 
@@ -82,6 +112,16 @@ TEST_GROUP_RUNNER(Radix_sort)
   RUN_TEST_CASE(Radix_sort, TestSort8);
   RUN_TEST_CASE(Radix_sort, TestSort9);
   RUN_TEST_CASE(Radix_sort, TestSort10);
+  RUN_TEST_CASE(Radix_sort, TestSort11);
+  RUN_TEST_CASE(Radix_sort, TestSort12);
+  RUN_TEST_CASE(Radix_sort, TestSort13);
+  RUN_TEST_CASE(Radix_sort, TestSort14);
+  RUN_TEST_CASE(Radix_sort, TestSort15);
+  RUN_TEST_CASE(Radix_sort, TestSort16);
+  RUN_TEST_CASE(Radix_sort, TestSort17);
+  RUN_TEST_CASE(Radix_sort, TestSort18);
+  RUN_TEST_CASE(Radix_sort, TestSort19);
+  RUN_TEST_CASE(Radix_sort, TestSort20);
 }
 
 TEST_GROUP_RUNNER(Merge_sort)
@@ -96,6 +136,16 @@ TEST_GROUP_RUNNER(Merge_sort)
   RUN_TEST_CASE(Merge_sort, TestSort8);
   RUN_TEST_CASE(Merge_sort, TestSort9);
   RUN_TEST_CASE(Merge_sort, TestSort10);
+  RUN_TEST_CASE(Merge_sort, TestSort11);
+  RUN_TEST_CASE(Merge_sort, TestSort12);
+  RUN_TEST_CASE(Merge_sort, TestSort13);
+  RUN_TEST_CASE(Merge_sort, TestSort14);
+  RUN_TEST_CASE(Merge_sort, TestSort15);
+  RUN_TEST_CASE(Merge_sort, TestSort16);
+  RUN_TEST_CASE(Merge_sort, TestSort17);
+  RUN_TEST_CASE(Merge_sort, TestSort18);
+  RUN_TEST_CASE(Merge_sort, TestSort19);
+  RUN_TEST_CASE(Merge_sort, TestSort20);
 }
 
 
@@ -111,6 +161,16 @@ TEST_GROUP_RUNNER(Heap_sort)
   RUN_TEST_CASE(Heap_sort, TestSort8);
   RUN_TEST_CASE(Heap_sort, TestSort9);
   RUN_TEST_CASE(Heap_sort, TestSort10);
+  RUN_TEST_CASE(Heap_sort, TestSort11);
+  RUN_TEST_CASE(Heap_sort, TestSort12);
+  RUN_TEST_CASE(Heap_sort, TestSort13);
+  RUN_TEST_CASE(Heap_sort, TestSort14);
+  RUN_TEST_CASE(Heap_sort, TestSort15);
+  RUN_TEST_CASE(Heap_sort, TestSort16);
+  RUN_TEST_CASE(Heap_sort, TestSort17);
+  RUN_TEST_CASE(Heap_sort, TestSort18);
+  RUN_TEST_CASE(Heap_sort, TestSort19);
+  RUN_TEST_CASE(Heap_sort, TestSort20);
 }
 
 TEST_GROUP_RUNNER(Quick_sort)
@@ -125,4 +185,14 @@ TEST_GROUP_RUNNER(Quick_sort)
   RUN_TEST_CASE(Quick_sort, TestSort8);
   RUN_TEST_CASE(Quick_sort, TestSort9);
   RUN_TEST_CASE(Quick_sort, TestSort10);
+  RUN_TEST_CASE(Quick_sort, TestSort11);
+  RUN_TEST_CASE(Quick_sort, TestSort12);
+  RUN_TEST_CASE(Quick_sort, TestSort13);
+  RUN_TEST_CASE(Quick_sort, TestSort14);
+  RUN_TEST_CASE(Quick_sort, TestSort15);
+  RUN_TEST_CASE(Quick_sort, TestSort16);
+  RUN_TEST_CASE(Quick_sort, TestSort17);
+  RUN_TEST_CASE(Quick_sort, TestSort18);
+  RUN_TEST_CASE(Quick_sort, TestSort19);
+  RUN_TEST_CASE(Quick_sort, TestSort20);
 }
