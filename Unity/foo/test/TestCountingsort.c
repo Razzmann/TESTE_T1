f0629a7639926af93eef205418b24d9eb05f9c8a
Razzmann <53624104+Razzmann@users.noreply.github.com>
2022-10-02 01:46:59 -0300

    Update TestCountingsort.c

--- Unity/foo/test/TestCountingsort.c
+++ Unity/foo/test/TestCountingsort.c
@@ -19,8 +19,8 @@ TEST_TEAR_DOWN(Counting_sort)
 TEST(Counting_sort, TestSort1) //TESTE DE VALOR LIMITE MAXIMO
 { 
 	
-	int a[] = {INT_MAX, 8, 7, 6, 5, 4, 3, 2, 1};
-	int b[] ={1,2,3,4,5,6,7,8,INT_MAX};
+	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+	int b[] ={1,2,3,4,5,6,7,8,9};
 	int length = sizeof(a) / sizeof(int);
 	char complex[]="On";
 	
