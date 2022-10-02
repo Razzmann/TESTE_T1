a81d5816e0efd3df89dd9b5c86e4d74bc9067c53
Razzmann <53624104+Razzmann@users.noreply.github.com>
2022-10-01 23:45:44 -0300

    Update TestMergesort.c

--- Unity/foo/test/TestMergesort.c
+++ Unity/foo/test/TestMergesort.c
@@ -16,7 +16,7 @@ TEST_TEAR_DOWN(Merge_sort)
 {
 }
 
-TEST(Selection_sort, TestSort1) //TESTE DE VALOR LIMITE MAXIMO
+TEST(Merge_sort, TestSort1) //TESTE DE VALOR LIMITE MAXIMO
 { 
 	int a[] = {INT_MAX, 8, 7, 6, 5, 4, 3, 2, 1};
 	int b[] ={1,2,3,4,5,6,7,8,INT_MAX};
@@ -28,7 +28,7 @@ TEST(Selection_sort, TestSort1) //TESTE DE VALOR LIMITE MAXIMO
 
 }
 
-TEST(Selection_sort, TestSort2) //TESTE DE VALOR LIMITE MINIMO
+TEST(Merge_sort, TestSort2) //TESTE DE VALOR LIMITE MINIMO
 {
 	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, INT_MIN};
 	int b[] ={INT_MIN,2,3,4,5,6,7,8,9};
@@ -42,7 +42,7 @@ TEST(Selection_sort, TestSort2) //TESTE DE VALOR LIMITE MINIMO
 	
 }
 
-TEST(Selection_sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
+TEST(Merge_sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
 {
 	int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 0};
 	int b[] ={0,2,3,4,5,6,7,8,9};
@@ -55,7 +55,7 @@ TEST(Selection_sort, TestSort3) //TESTE DE VALOR LIMITE (ZERO)
 	
 }
 
-TEST(Selection_sort, TestSort4) //TESTE DE VALORES REPETIDOS E EM ORDEM
+TEST(Merge_sort, TestSort4) //TESTE DE VALORES REPETIDOS E EM ORDEM
 {
 	int a[] = {0 , 0, 1, 1, 2, 2, 3, 3, 4, 4};
 	int b[] = {0 , 0, 1, 1, 2, 2, 3, 3, 4, 4};
@@ -69,7 +69,7 @@ TEST(Selection_sort, TestSort4) //TESTE DE VALORES REPETIDOS E EM ORDEM
 	
 }
 
-TEST(Selection_sort, TestSort5)//TESTE DE VALOR LIMITE metade de min e max
+TEST(Merge_sort, TestSort5)//TESTE DE VALOR LIMITE metade de min e max
 {
 	int a[] = {INT_MAX/2, 8, 7, 6, 5, 4, 3, 2, INT_MIN/2};
 	int b[] ={INT_MIN/2,2,3,4,5,6,7,8,INT_MAX/2};
@@ -81,7 +81,7 @@ TEST(Selection_sort, TestSort5)//TESTE DE VALOR LIMITE metade de min e max
 	
 }
 
-TEST(Selection_sort, TestSort6)//TESTE DE VALORES JÁ ORDENADOS
+TEST(Merge_sort, TestSort6)//TESTE DE VALORES JÁ ORDENADOS
 {
 	int a[] = {-767, 125, 345 , 367, 421, 5743, 6612, 7012, 8999,9999 , 10000, 11142, 12928,91023,123507,1681223};
 	int b[] = {-767, 125, 345 , 367, 421, 5743, 6612, 7012, 8999,9999 , 10000, 11142, 12928,91023,123507,1681223};
@@ -95,7 +95,7 @@ TEST(Selection_sort, TestSort6)//TESTE DE VALORES JÁ ORDENADOS
 
 	
 }
-TEST(Selection_sort, TestSort7)//TESTE DE VALORES IGUAIS
+TEST(Merge_sort, TestSort7)//TESTE DE VALORES IGUAIS
 {
 	int a[] = {92458, 92458, 92458 , 92458, 92458, 92458, 92458, 92458, 92458,92458 , 92458, 92458, 92458,92458,92458,92458};
 	int b[] = {92458, 92458, 92458 , 92458, 92458, 92458, 92458, 92458, 92458,92458 , 92458, 92458, 92458,92458,92458,92458};
@@ -108,7 +108,7 @@ TEST(Selection_sort, TestSort7)//TESTE DE VALORES IGUAIS
 	
 }
 
-TEST(Selection_sort, TestSort8)//TESTE ARRAY OVERFLOW
+TEST(Merge_sort, TestSort8)//TESTE ARRAY OVERFLOW
 {
 	int a[] = {21,20,19,18,17,16,15,14,13,12,11,10,9, 8, 7, 6, 5, 4, 3, 2, 1};
 	int length = sizeof(a) / sizeof(int);
@@ -118,7 +118,7 @@ TEST(Selection_sort, TestSort8)//TESTE ARRAY OVERFLOW
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, MERGE));		
 	
 }
-TEST(Selection_sort, TestSort9)//TESTE ARRAY UNDERFLOW
+TEST(Merge_sort, TestSort9)//TESTE ARRAY UNDERFLOW
 {
 	int a[] = {211};
 	int length = sizeof(a) / sizeof(int);
@@ -127,7 +127,7 @@ TEST(Selection_sort, TestSort9)//TESTE ARRAY UNDERFLOW
 	
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, MERGE));
 }
-TEST(Selection_sort, TestSort10)//TESTE ARRAY ZERADO
+TEST(Merge_sort, TestSort10)//TESTE ARRAY ZERADO
 {
 	int a[] = {};
 	int length = sizeof(a) / sizeof(int);
@@ -137,14 +137,14 @@ TEST(Selection_sort, TestSort10)//TESTE ARRAY ZERADO
 
 
 
-TEST(Selection_sort, TestSort11)//TESTE ARRAY PASSANDO METADE 11
+TEST(Merge_sort, TestSort11)//TESTE ARRAY PASSANDO METADE 11
 {
 	int a[] = {123,0,12512,67213,-128743,7463,1324,7542,134,-1246423,-19};
 	int length = sizeof(a) / sizeof(int);
 	char complex[]="Onlogn";
 	TEST_ASSERT_EQUAL(0, sort(a, length, complex, MERGE));
 }
-TEST(Selection_sort, TestSort12)//TESTE PASSANDO MÁXIMO
+TEST(Merge_sort, TestSort12)//TESTE PASSANDO MÁXIMO
 {
 	int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 	int length = sizeof(a) / sizeof(int);
@@ -154,7 +154,7 @@ TEST(Selection_sort, TestSort12)//TESTE PASSANDO MÁXIMO
  TEST_ASSERT_EQUAL(0, sort(a, length, complex, MERGE));
 }
 
-TEST(Selection_sort, TestSort13)//TESTE PASSANDO MÍNIMO
+TEST(Merge_sort, TestSort13)//TESTE PASSANDO MÍNIMO
 {
 	int a[] = {1,2};
 	int length = sizeof(a) / sizeof(int);
@@ -164,7 +164,7 @@ TEST(Selection_sort, TestSort13)//TESTE PASSANDO MÍNIMO
  TEST_ASSERT_EQUAL(0, sort(a, length, complex, MERGE));
 }
 
-TEST(Selection_sort, TestSort14)//TESTE COMPLEXIDADE ERRADA
+TEST(Merge_sort, TestSort14)//TESTE COMPLEXIDADE ERRADA
 {
 	int a[] = {-176234,561,1112,-5671,85124,5819};
 	int length = sizeof(a) / sizeof(int);
@@ -174,7 +174,7 @@ TEST(Selection_sort, TestSort14)//TESTE COMPLEXIDADE ERRADA
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, MERGE));
 
 }
-TEST(Selection_sort, TestSort15)//TESTE COMPLEXIDADE ERRADA
+TEST(Merge_sort, TestSort15)//TESTE COMPLEXIDADE ERRADA
 {
 	int a[] = {-176234,561,1112,-5671,85124,5819};
 	int length = sizeof(a) / sizeof(int);
@@ -184,7 +184,7 @@ TEST(Selection_sort, TestSort15)//TESTE COMPLEXIDADE ERRADA
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, MERGE));
 
 }
-TEST(Selection_sort, TestSort16)//TESTE COMPLEXIDADE ERRADA
+TEST(Merge_sort, TestSort16)//TESTE COMPLEXIDADE ERRADA
 {
 	int a[] = {-176234,561,1112,-5671,85124,5819};
 	int length = sizeof(a) / sizeof(int);
@@ -193,7 +193,7 @@ TEST(Selection_sort, TestSort16)//TESTE COMPLEXIDADE ERRADA
 	
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, MERGE));
 }
-TEST(Selection_sort, TestSort17)//TESTE COMPLEXIDADE ERRADA
+TEST(Merge_sort, TestSort17)//TESTE COMPLEXIDADE ERRADA
 {
 	int a[] = {-176234,561,1112,-5671,85124,5819};
 	int length = sizeof(a) / sizeof(int);
@@ -203,7 +203,7 @@ TEST(Selection_sort, TestSort17)//TESTE COMPLEXIDADE ERRADA
  TEST_ASSERT_EQUAL(1, sort(a, length, complex, MERGE));
 }
 
-TEST(Selection_sort, TestSort18)//TESTE CASE SENSITIVE
+TEST(Merge_sort, TestSort18)//TESTE CASE SENSITIVE
 {
 	int a[] = {-176234,561,1112,-5671,85124,5819};
 	int length = sizeof(a) / sizeof(int);
@@ -214,11 +214,11 @@ TEST(Selection_sort, TestSort18)//TESTE CASE SENSITIVE
 }
 
 
-TEST(Selection_sort, TestSort19)
+TEST(Merge_sort, TestSort19)
 {
 	
 }
-TEST(Selection_sort, TestSort20){}
+TEST(Merge_sort, TestSort20){}
 
 
 
