170e1afe65737ed795b65a16958620a6b37f3744
digocz <digo_zaparoli@hotmail.com>
2022-10-05 21:36:34 -0300

    Update TestInsertionsort.c

--- Unity/foo/test/TestInsertionsort.c
+++ Unity/foo/test/TestInsertionsort.c
@@ -218,6 +218,7 @@ TEST(Insertion_sort, TestSort21)
 {
 	int a[] = {0,12512,67213,-128743,7463,1324,7542,134,-1246423,-19};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="On2";
 	TEST_ASSERT_EQUAL(1, sort(a, length, complex, 8));
 	TEST_ASSERT_EQUAL(1, sort(a, length, complex, -1));
 	
