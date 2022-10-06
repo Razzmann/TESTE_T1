1e126189b4ef860aa1ea98badf384514bed04549
digocz <digo_zaparoli@hotmail.com>
2022-10-05 21:36:18 -0300

    Update TestHeapsort.c

--- Unity/foo/test/TestHeapsort.c
+++ Unity/foo/test/TestHeapsort.c
@@ -265,6 +265,7 @@ TEST(Heap_sort, TestSort21)
 {
 	int a[] = {0,12512,67213,-128743,7463,1324,7542,134,-1246423,-19};
 	int length = sizeof(a) / sizeof(int);
+	char complex[]="Onlogn";
 	TEST_ASSERT_EQUAL(1, sort(a, length, complex, 8));
 	TEST_ASSERT_EQUAL(1, sort(a, length, complex, -1));
 	
